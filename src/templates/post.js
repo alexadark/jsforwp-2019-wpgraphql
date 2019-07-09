3d6bed1b4384de51c1be7b4e9fa06c6d20a6aac1
alexandra <alexaspalato@gmail.com>
2019-07-09 15:34:06 +0200

    act fields

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,7 +3,12 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 
 const SinglePOST = ({ data }) => {
-  const { content, featuredImage, title } = data.wpgraphql.post
+  const {
+    content,
+    featuredImage,
+    title,
+    acfDemoFields: { flexibleField, myTextField },
+  } = data.wpgraphql.post
 
   return (
     <Layout>
@@ -12,7 +17,29 @@ const SinglePOST = ({ data }) => {
       )}
 
       <h1 className="page-title" dangerouslySetInnerHTML={{ __html: title }} />
+      <h2>{myTextField}</h2>
       <p className="content" dangerouslySetInnerHTML={{ __html: content }} />
+      {flexibleField.length > 0 &&
+        flexibleField.map(layout => {
+          switch (layout.__typename) {
+            case "WPGraphQL_Post_Acfdemofields_FlexibleField_TextareaBlock":
+              return (
+                <>
+                  <h3>Textarea</h3>
+                  <p>{layout.textarea}</p>
+                </>
+              )
+
+            case "WPGraphQL_Post_Acfdemofields_FlexibleField_ImageBlock":
+              return (
+                <>
+                  <h3>Text Image block</h3>
+                  <img src={layout.image.sourceUrl} alt="" />
+                  <h4>{layout.text}</h4>
+                </>
+              )
+          }
+        })}
     </Layout>
   )
 }
@@ -27,6 +54,21 @@ export const pageQuery = graphql`
         featuredImage {
           sourceUrl
           altText
+        }
+        acfDemoFields {
+          myTextField
+          flexibleField {
+            __typename
+            ... on WPGraphQL_Post_Acfdemofields_FlexibleField_ImageBlock {
+              text
+              image {
+                sourceUrl
+              }
+            }
+            ... on WPGraphQL_Post_Acfdemofields_FlexibleField_TextareaBlock {
+              textarea
+            }
+          }
         }
       }
     }
